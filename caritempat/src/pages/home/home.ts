a2180e04967307de5e8b44a9c96b6bbfcdfcb278
tiyassafira <tsfiraa19@gmail.com>
2017-08-14 11:33:25 +0700

    first commit

---
+++ caritempat/src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { NewPlacePage } from "../new-place/new-place";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  onLoadNewPlace(){
+  this.navCtrl.push(NewPlacePage);
+  }
+
+}
---
+++ caritempat/src/pages/new-place/new-place.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';	
+
+@Component({
+  selector: 'page-new-place',
+  templateUrl: 'new-place.html',
+})
+export class NewPlacePage {
+
+  constructor() {}
+  onAddPlace(value: {title: string}) {
+
+  
+  }
+
+}
